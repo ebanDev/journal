277d72f44d8351a8584d2e7fbba92c6f813fdd4d
Eban <contact@eban.eu.org>
2025-08-06 23:25:14 +0200

    feat: extend Json type to support additional data structures

--- types/database.types.ts
+++ types/database.types.ts
@@ -1,3 +1,11 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
 export type Database = {
   // Allows to automatically instanciate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
